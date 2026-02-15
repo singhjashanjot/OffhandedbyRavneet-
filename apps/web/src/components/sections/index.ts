f17e19e4727a45ae454b3b37f0fb202c282f7c1d
Jashanjot Singh <jsjsnarang6@gmail.com>
2026-02-15 21:22:15 +0530

    feat(sections): add index file for exporting section components

---
+++ apps/web/src/components/sections/index.ts
@@ -0,0 +1,12 @@
+/* ========================================
+   SECTION COMPONENTS EXPORTS
+======================================== */
+
+export { HeroSection } from "./HeroSection";
+export { CategoriesSection } from "./CategoriesSection";
+export { UpcomingWorkshopsSection } from "./UpcomingWorkshopsSection";
+
+export { GalleryPreviewSection } from "./GalleryPreviewSection";
+export { TestimonialsSection } from "./TestimonialsSection";
+export { FounderSection } from "./FounderSection";
+export { CTASection } from "./CTASection";
