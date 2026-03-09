d7425bc1876ecc6b9c7967999e9abc612bfba931
Jashanjot Singh <jsjsnarang6@gmail.com>
2026-03-10 03:23:25 +0530

    feat(next-config): add remote pattern for lh3.googleusercontent.com to image configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "**.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
